29c9e728708d6caf08ae3a2e9cae6d3175ad8ce6
KaiWei Shen <keshen@ucdavis.edu>
2020-09-05 09:15:17 -0700

    change App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Search from "./pages/Search";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Porfolio" component={Portfolio} />
+          <Route exact path="/Search" component={Search} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
