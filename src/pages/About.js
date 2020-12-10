4f36a5b44610f0f73a28ffed427285e3c1aecd74
KaiWei Shen <keshen@ucdavis.edu>
2020-12-10 12:01:06 -0800

    organize

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,24 +19,22 @@ function About() {
 
             <div className="kaiwei">
               <img src={proflie} className="profile" alt="KaiWei" />
-            </div>
 
-            <div className="paragraph">
               <p>
                 Welcome! Thank you for taking the time out of your day to view my
                 progress so far. My name is KaiWei Edward Shen, I graduated from UC Davis with Food Science and Technology degree. I decided to learn coding to expand my skill set and challenge myself in a field I had no experience until recently. My reasoning for entering coding bootcamp is seeing better opprotunity for career as coding is an ever expanding market I want to be part of.
-            </p>
+              </p>
 
               <p>
                 I've recently finished my stint at U.C. Davis and
                 persuing a career as a full stack web developer. Throughout my time with U.C Davis, I've gained experience on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, Node Js, MySQL, MongoDB, Express, Handelbars.js & ReactJS. Although it was quite challenging, I look forward to use and
                 learn more with the current languages I am familiar with and new languages to create software products.
-            </p>
+              </p>
 
               <p>
                 Please explore the other pages and the work I have put into my portfolio. I have several projects with different types of coding structures to them. The first row focuses on basic HTML, CSS, and Javascript.
                 The middle has more variety, using backend coding and handlebars for the project. Last project uses React as the template.
-            </p>
+              </p>
             </div>
 
           </Col>
