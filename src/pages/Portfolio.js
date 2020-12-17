26d7d3f97f91679aa02e22fb9829d59715adce2f
KaiWei Shen <keshen@ucdavis.edu>
2020-12-17 08:57:46 -0800

    listing

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -44,8 +44,8 @@ class Portfolio extends Component {
             <a href="https://github.com/kaishen33/Spotify-Artist-Search" className="btn btn-primary">Github Link</a>
             <a href="https://kaishen33.github.io/Spotify-Artist-Search/index.html" className="btn btn-primary">View Page</a>
             <p>
-              Search artists, songs, an albums associated with each other.
-              - HTML, CSS, Javascript, AJAX, JQuery
+              Search artists, songs, an albums associated with each other. </p>
+            <p>  - HTML, CSS, Javascript, AJAX, JQuery
             </p>
           </div>
 
@@ -54,8 +54,8 @@ class Portfolio extends Component {
             <a href="https://github.com/kaishen33/schedulemaker" className="btn btn-primary">Github Link</a>
             <a href="https://kaishen33.github.io/schedulemaker/index.html" className="btn btn-primary">View Page</a>
             <p>
-              A way to schedule your day.
-              -HTML, CSS, Javascript
+              A way to schedule your day. </p>
+            <p>  -HTML, CSS, Javascript
               </p>
           </div>
 
@@ -64,8 +64,8 @@ class Portfolio extends Component {
             <a href="https://github.com/kaishen33/weather" className="btn btn-primary">Github Link</a>
             <a href="https://kaishen33.github.io/weather/index.html" className="btn btn-primary">View Page</a>
             <p>
-              City weather forecast application
-              - HTML, Javascript
+              City weather forecast application </p>
+            <p>  - HTML, Javascript
               </p>
           </div>
 
@@ -99,8 +99,8 @@ class Portfolio extends Component {
             <a href="https://github.com/kaishen33/Server" className="btn btn-primary">Github Link</a>
             <a href="https://kai-notes-db-hw.herokuapp.com/" className="btn btn-primary">View Page</a>
             <p>
-              Notetaking application
-              - HTML, CSS, Javascript, MySQL, Node
+              Notetaking application </p>
+            <p>  - HTML, CSS, Javascript, MySQL, Node
             </p>
           </div>
 
@@ -108,10 +108,8 @@ class Portfolio extends Component {
             <h5 className="card-title">Employee Profiling</h5>
             <a href="https://github.com/kaishen33/OOP" className="btn btn-primary">Github Link</a>
             <a href="https://kaishen33.github.io/OOP/output/team.html" className="btn btn-primary">View Page</a>
-            <p>
-              Employee profile generator
-              - HTML, Javascript, Node
-            </p>
+            <p> Employee profile generator </p>
+            <p>  - HTML, Javascript, Node</p>
           </div>
 
           <div className="card-body col-md-4">
@@ -119,8 +117,8 @@ class Portfolio extends Component {
             <a href="https://beyondvacation.herokuapp.com/" className="btn btn-primary">Github Link</a>
             <a href="https://github.com/vanessabau/projectTwo" className="btn btn-primary">View Page</a>
             <p>
-              Outdoor rental application
-              -Handlebar, Javascript, MySQL, CSS, JQuery
+              Outdoor rental application </p>
+            <p>  -Handlebar, Javascript, MySQL, CSS, JQuery
               </p>
           </div>
 
@@ -154,8 +152,8 @@ class Portfolio extends Component {
             <a href="https://github.com/kaishen33/mongodb" className="btn btn-primary">Github Link</a>
             <a href="https://kai-fitness-tracker-hw.herokuapp.com/?id=5f2caaf249a8574560f9cbb1" className="btn btn-primary">View Page</a>
             <p>
-              Exercise planner and performance tracker
-              - Mongodb
+              Exercise planner and performance tracker </p>
+            <p>  - Mongodb
             </p>
           </div>
 
@@ -164,8 +162,8 @@ class Portfolio extends Component {
             <a href="https://github.com/kaishen33/react" className="btn btn-primary">Github Link</a>
             <a href=" https://react-employee-filter.herokuapp.com/" className="btn btn-primary">View Page</a>
             <p>
-              List organizer application
-              -  React, Javascript
+              List organizer application </p>
+            <p>  -  React, Javascript
             </p>
           </div>
 
@@ -174,8 +172,8 @@ class Portfolio extends Component {
             <a href="https://github.com/kaishen33/Project3" className="btn btn-primary">Github Link</a>
             <a href="https://dry-mountain-58151.herokuapp.com/" className="btn btn-primary">View Page</a>
             <p>
-              Free to play game application
-              - React, Mongodb, Javascript, CSS, Node
+              Free to play game application </p>
+            <p>  - React, Mongodb, Javascript, CSS, Node
             </p>
           </div>
 
